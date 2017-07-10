1e759c97b1753be98f50f6a16cac2d8f1c6da795
nandate <o238049t@mail.kyutech.jp>
2017-07-10 16:39:30 +0900

    user model(CRUD)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
     json_response(@users)
   end
 
   def create
-    @user = User.create!(user_params)
-    json_response(@user, :created)
+    @user = User.new(user_params)
+    if @user.save
+      json_response(@user, :created)
+    else
+      json_response(@user.errors, :unprocessable_entity)
+    end
   end
 
   def show
     json_response(@user)
+  end
+
+  def update
+    if @user.update(user_params)
+      json_response(@user)
+    else
+      json_response(@user.errors,:unprocessable_entity)
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
   end
 
   private
